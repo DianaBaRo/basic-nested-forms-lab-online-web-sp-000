4b905c202871bfe4b864c1742c2f9816f17d80d3
Diana Badas <diana.badas@gmail.com>
2019-06-03 10:44:07 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,12 +21,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  def update
-    recipe = Recipe.find(params[:id])
-    recipe.update(recipe_params)
-    redirect_to recipe
-  end
-
   private
 
   def recipe_params
